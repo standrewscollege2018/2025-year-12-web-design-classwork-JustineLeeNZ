9ffa8e4bdf888babe1f19ef205373bd84db27f09
JustineLeeNZ <jlu@stac.school.nz>
2025-03-03 12:26:26 +1300

    PHP Basics
    echo, variables, get

---
+++ php-intro/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo of PHP basics</title>
+</head>
+<body>
+    <p>This is a PHP page</p>
+
+    <?php
+
+    // write output using echo
+    echo "Hello World!";
+
+    // create a variable
+    $customer_name = "Bob The Builder";
+
+    // write out a variable
+    echo "<p>Hello $customer_name</p>";
+
+    ?>
+
+    <a href="name.php?name=Angus">Angus</a>
+    <a href="name.php?name=Josh">Josh</a>
+
+</body>
+</html>
